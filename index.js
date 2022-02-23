efa17b09043c71cb412dfe160679ef4f1f6596e7
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-23 03:18:56 -0800

    Actually add time

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+function timeToGo(s) {
+  var now = new Date;
+  var utc_timestamp = new Date(Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() ,
+      now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()));
+  var diff = new Date(s.getTime() - utc_timestamp.getTime());
+
+
+  out = '';
+  out += diff.getHours() + " hours, ";
+  out += diff.getMinutes() + " minutes, "
+  out += diff.getSeconds() + " seconds"
+
+
+  return out;
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let dropsTime = new Date('2022-02-23T16:00:00+0000');
+
+
+  res.send('Hunt Drops in: ' + timeToGo(dropsTime));
 });
 
 app.listen(PORT, () => {
